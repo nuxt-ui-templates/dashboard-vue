4bd07b18bab186c34cd2556aebee2cba6eb25699
Benjamin Canac <canacb1@gmail.com>
2025-03-07 17:55:17 +0100

    chore: lint

--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -8,10 +8,6 @@ defineProps<{
 }>()
 
 const colorMode = useColorMode()
-const appConfig = useAppConfig()
-
-const colors = ['red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose']
-const neutrals = ['slate', 'gray', 'zinc', 'neutral', 'stone']
 
 const user = ref({
   name: 'Benjamin Canac',
