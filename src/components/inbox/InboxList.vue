98d13564e33cb1232a6d7e32179d261cebc4d06f
Benjamin Canac <canacb1@gmail.com>
2025-10-23 17:07:20 +0200

    chore(inbox): align with nuxt dashboard
    https://github.com/nuxt-ui-templates/dashboard/pull/234

--- src/components/inbox/InboxList.vue
+++ src/components/inbox/InboxList.vue
@@ -54,7 +54,9 @@ defineShortcuts({
         class="p-4 sm:px-6 text-sm cursor-pointer border-l-2 transition-colors"
         :class="[
           mail.unread ? 'text-highlighted' : 'text-toned',
-          selectedMail && selectedMail.id === mail.id ? 'border-primary bg-primary/10' : 'border-(--ui-bg) hover:border-primary hover:bg-primary/5'
+          selectedMail && selectedMail.id === mail.id
+            ? 'border-primary bg-primary/10'
+            : 'border-(--ui-bg) hover:border-primary hover:bg-primary/5'
         ]"
         @click="selectedMail = mail"
       >
