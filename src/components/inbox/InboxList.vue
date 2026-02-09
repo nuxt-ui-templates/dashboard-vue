77ccb0b3f701f19c3d2e708269535ade7126b40f
Benjamin Canac <canacb1@gmail.com>
2026-02-09 14:39:15 +0100

    up

--- src/components/inbox/InboxList.vue
+++ src/components/inbox/InboxList.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
   mails: Mail[]
 }>()
 
-const mailsRefs = ref<Element[]>([])
+const mailsRefs = ref<Record<number, Element | null>>({})
 
 const selectedMail = defineModel<Mail | null>()
 
@@ -48,7 +48,7 @@ defineShortcuts({
     <div
       v-for="(mail, index) in mails"
       :key="index"
-      :ref="el => { mailsRefs[mail.id] = el as Element }"
+      :ref="(el) => { mailsRefs[mail.id] = el as Element | null }"
     >
       <div
         class="p-4 sm:px-6 text-sm cursor-pointer border-l-2 transition-colors"
