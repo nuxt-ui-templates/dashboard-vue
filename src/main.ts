bb4f5452a8b9ceb5693c7cd66821ebd88f957914
Benjamin Canac <canacb1@gmail.com>
2026-03-31 12:27:15 +0200

    feat: prevent dark mode flash on page refresh

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,20 @@ import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
+import { createHead } from '@unhead/vue/client'
 import ui from '@nuxt/ui/vue-plugin'
 
 import App from './App.vue'
 
 const app = createApp(App)
 
+const head = createHead()
 const router = createRouter({
   routes: setupLayouts(routes as RouteRecordRaw[]),
   history: createWebHistory()
 })
 
+app.use(head)
 app.use(router)
 app.use(ui)
 
